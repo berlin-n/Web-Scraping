4b1792899d08bf79d702621c169b8427a79e2459
berlin-n <130678445+berlin-n@users.noreply.github.com>
2025-01-10 13:58:44 +0100

    Fourth Commit: Using the html elements of the html we want to scrape, to scrape the website. Also made some changes to the lxml parsing.

--- BeautifulSoup/CryptoDataScraper/main.py
+++ BeautifulSoup/CryptoDataScraper/main.py
@@ -6,4 +6,7 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
 }
 
-html_text = requests.get('https://finance.yahoo.com/markets/crypto/gainers/', 'lxml')
+html_text = requests.get('https://finance.yahoo.com/markets/crypto/gainers/').text
+soup = BeautifulSoup(html_text, 'lxml')
+token = soup.find('tr', class_="row false yf-paf8n5")
+print(token)
