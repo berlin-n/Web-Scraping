d3ff3944713f41232f30e52367b9ba3ad3b369db
berlin-n <130678445+berlin-n@users.noreply.github.com>
2025-01-10 15:55:10 +0100

    Made the core part of the code a function, to be accessible by the url that wish to be scraped

--- BeautifulSoup/CryptoDataScraper/main.py
+++ BeautifulSoup/CryptoDataScraper/main.py
@@ -6,12 +6,15 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
 }
 
-html_text = requests.get('https://finance.yahoo.com/markets/crypto/gainers/', headers).text
-soup = BeautifulSoup(html_text, 'lxml')
-tokens  = soup.find_all('tr', class_="row false yf-paf8n5")
-for index, token in enumerate(tokens):
-    name = token.find('div', class_='yf-eg2gbv').text
-    symbol = token.find('span', class_='symbol yf-1m808gl').text
-    marketCap = token.find('fin-streamer', {'data-field': 'marketCap'}).text
-    percentageChange = token.find('fin-streamer', {'data-field': 'regularMarketChangePercent'}).text
-    print(f'{name},({symbol}) current Market Cap is {marketCap} and rose by {percentageChange}')
+def scrape(url):
+    html_text = requests.get(url, headers).text
+    soup = BeautifulSoup(html_text, 'lxml')
+    tokens  = soup.find_all('tr', class_="row false yf-paf8n5")
+    for index, token in enumerate(tokens):
+        name = token.find('div', class_='yf-eg2gbv').text
+        symbol = token.find('span', class_='symbol yf-1m808gl').text
+        marketCap = token.find('fin-streamer', {'data-field': 'marketCap'}).text
+        percentageChange = token.find('fin-streamer', {'data-field': 'regularMarketChangePercent'}).text
+        print(f'{name},({symbol}) current Market Cap is {marketCap} and rose by {percentageChange}')
+
+scrape('https://finance.yahoo.com/markets/crypto/gainers/')
